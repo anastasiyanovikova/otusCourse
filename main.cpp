12ba0c76bd33b10cceaecd4a483fdee36b1ced56
anastasiyanovikova <34402531+anastasiyanovikova@users.noreply.github.com>
2024-01-27 17:36:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main (int, char **) {
-    std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "build " << version() << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
