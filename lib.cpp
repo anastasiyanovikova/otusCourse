5b3c80dff5680ec249605f19b5ebcace79206252
anastasiyanovikova <novikovi.2107@yandex.ru>
2024-02-11 22:08:00 +0300

    task 2

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,98 @@
 #include "lib.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-#include "version.h"
+std::vector<int> strVectorToInt(std::vector<std::string> strItems)
+{
+    std::vector<int> newItems;
+    for(auto it = strItems.cbegin(); it != strItems.cend(); ++it)
+    {
+        newItems.push_back(std::stoi(*it));
+    }
+    return newItems;
+}
+bool sortIp(std::vector<std::vector<int>>& items)
+{
+    std::sort(items.begin(), items.end(), [](std::vector<int>firstEl, std::vector<int>SecondEl){
+        auto firstS = firstEl.size();
+        auto secondS = SecondEl.size();
+        auto minS = firstS < secondS ? firstS : secondS;
+        for(long unsigned int i = 0; i < minS; ++i)
+        {
+            if(firstEl.at(i) < SecondEl.at(i))
+                return false;
+            if(firstEl.at(i) > SecondEl.at(i))
+                return true;
+        }
+        if(firstS < secondS)
+            return false;
+        return true;
+    });
+    return true;
+}
+bool filter(const std::vector<std::vector<int>>& items, int value)
+{
+    for(auto it = items.cbegin(); it != items.cend(); ++it)
+    {
+        if(it->size() > 0 && it->at(0) == value)
+        {
+            for(auto ip_part = it->cbegin(); ip_part != it->cend(); ++ip_part)
+            {
+                if (ip_part != it->cbegin())
+                {
+                    std::cout << ".";
 
-int version() {
-    return PROJECT_VERSION_PATCH;
+                }
+                std::cout << *ip_part;
+            }
+            std::cout << std::endl;
+        }
+    }
+    return true;
+}
+bool filter(const std::vector<std::vector<int>>& items, int valueFirst, int valueSecond)
+{
+    for(auto it = items.cbegin(); it != items.cend(); ++it)
+    {
+        if(it->size() > 1 && it->at(0) == valueFirst && it->at(1) == valueSecond)
+        {
+            for(auto ip_part = it->cbegin(); ip_part != it->cend(); ++ip_part)
+            {
+                if (ip_part != it->cbegin())
+                {
+                    std::cout << ".";
+
+                }
+                std::cout << *ip_part;
+            }
+            std::cout << std::endl;
+        }
+    }
+    return true;
+}
+bool filter_any(const std::vector<std::vector<int>>& items, int value)
+{
+    for(auto it = items.cbegin(); it != items.cend(); ++it)
+    {
+        for(auto itIp = it->cbegin(); itIp != it->cend(); ++ itIp)
+        {
+            if(*itIp == value)
+            {
+                for(auto ip_part = it->cbegin(); ip_part != it->cend(); ++ip_part)
+                {
+                    if (ip_part != it->cbegin())
+                    {
+                        std::cout << ".";
+
+                    }
+                    std::cout << *ip_part;
+                }
+                std::cout << std::endl;
+                break;
+            }
+        }
+    }
+    return true;
 }
--- lib.h
+++ lib.h
@@ -1,3 +1,9 @@
 #pragma once
-
-int version();
+#include <vector>
+#include <iostream>
+#include<string>
+std::vector<int>strVectorToInt(std::vector<std::string> strItems);
+bool sortIp(std::vector<std::vector<int>>& items);
+bool filter(const std::vector<std::vector<int>>& items, int value);
+bool filter(const std::vector<std::vector<int>>& items, int valueFirst, int valueSecond);
+bool filter_any(const std::vector<std::vector<int>>& items, int value);
